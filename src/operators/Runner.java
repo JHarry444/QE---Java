39166edd7f06cdb5c982d7f93a8aeebf2de63025
jharry444 <jharrison1@qa.com>
2024-10-08 11:29:20 +0100

    operators

---
+++ src/operators/Runner.java
@@ -0,0 +1,23 @@
+package operators;
+
+public class Runner {
+
+    public static void main(String[] args) {
+        System.out.println(2 + 2);
+        System.out.println(2 - 2);
+        System.out.println(2 * 2);
+        System.out.println(2 / 2);
+        System.out.println(9 % 2);
+
+
+        System.out.println(Math.pow(3, 2));
+
+        System.out.println(10 > 3);
+        System.out.println(10 < 3);
+
+        System.out.println(10 == (5 + 5));
+        // ! -> NOT
+        System.out.println(10 != (5 + 5));
+        System.out.println(!true);
+    }
+}
