f05c3ad3a33bee55a81aa4ba016174b53dfd6230
jharry444 <jharrison1@qa.com>
2024-10-08 12:12:46 +0100

    variables

---
+++ src/variables/Runner.java
@@ -0,0 +1,38 @@
+package variables;
+
+public class Runner {
+    public static void main(String[] args) {
+        int x = 27;
+        System.out.println();
+
+        System.out.println(Byte.MAX_VALUE);
+        System.out.println(Long.MAX_VALUE);
+
+        boolean bool = false;
+        byte b = 12;
+        short s = 112;
+        int i = 256;
+        long l = 23432433453545345L;
+        float f = 12.9F;
+        double d = 43453453.434535;
+        char c = 'J';
+        String string = "Jordan Harrison";
+
+        System.out.println('j' == 'J');
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(999_999_999);
+        System.out.println("999_999_999");
+        System.out.println(5 / 2F);
+
+        i = i + 1;
+
+        System.out.println(i);
+
+        i++;
+        System.out.println(i);
+
+        System.out.println(i++);
+        System.out.println(++i);
+    }
+}
