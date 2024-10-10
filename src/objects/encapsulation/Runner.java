a78597e3d781f7cda5c1d6f351a00efcd2d1b033
jharry444 <jharrison1@qa.com>
2024-10-10 09:42:01 +0100

    constructors

--- src/objects/encapsulation/Runner.java
+++ src/objects/encapsulation/Runner.java
@@ -1,5 +1,6 @@
 package objects.encapsulation;
 
+import objects.Cat;
 import objects.Dog;
 
 public class Runner {
@@ -7,8 +8,22 @@ public class Runner {
     public static void main(String[] args) {
         Dog d = new Dog();
         d.setAge(-12);
-        d.name = "Barry";
+//        d.name = "Barry";
 //        d.breed = "?";
         System.out.println(d.getInfo());
+
+//        Cat c = new Cat();
+        Cat c = new Cat("Fluffy", 12, "Siamese");
+//        c.setAge(12);
+//        c.setBreed("Siamese");
+//        c.setName("Fluffy");
+
+        c.meow();
+
+        System.out.println(c.getInfo());
+
+
+
+
     }
 }
