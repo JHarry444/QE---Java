48b6bbf8b800ccbec643df17d1e10291636a05de
jharry444 <jharrison1@qa.com>
2024-10-10 10:29:33 +0100

    inheritance

--- src/objects/Dog.java
+++ src/objects/Dog.java
@@ -1,43 +1,11 @@
 package objects;
 
-public class Dog {
-    private String name;
-    private int age;
-    private String breed;
+import objects.inheritance.Pet;
 
+public class Dog extends Pet {
     void bark() {
         System.out.println("Woof");
     }
 
-    public String getInfo() {
-        return "My name is " + name + " and I am a " + age + " years old " + breed;
-    }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        if (age > 0) {
-            this.age = age;
-        } else {
-            System.out.println("Invalid age: " + age);
-        }
-    }
-
-    public String getBreed() {
-        return breed;
-    }
-
-    public void setBreed(String breed) {
-        this.breed = breed;
-    }
 }
